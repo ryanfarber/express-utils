0baaecd06368ccc1aa7cd9eef8bed2612c59b79d
Ryan Forever <32393930+ryanfarber@users.noreply.github.com>
2023-10-11 16:09:53 -0700

    update

--- src/route-logger.js
+++ src/route-logger.js
@@ -3,7 +3,22 @@
 const Logger = require("@ryanforever/logger").v2
 const logger = new Logger(__filename, {debug: false, style: 0})
 
-module.exports = function(req, res, next) {
-	logger.info(`↔ ${req.method}${req.path}`)
-	next()
+
+
+routeLogger(config = {}) {
+	let ignore = config.ignore ?? []
+	ignore = ignore.map(x => {
+		if (!x.startsWith("/")) x = `/${x}`
+		x = x.replace(/\/+/gi, "/")
+		return x
+	})
+	console.log(ignore)
+	return function(req, res, next) {
+		if (ignore.includes)
+		logger.info(`↔ ${req.method}${req.path}`)
+		next()
+	}
 }
+
+
+module.exports = routeLogger
